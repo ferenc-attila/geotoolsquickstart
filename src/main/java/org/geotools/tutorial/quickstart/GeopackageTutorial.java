9b093e502383aca54cb5164885e01b489b3b0170
ferenc-attila <attila.ferenc.80@gmail.com>
2022-03-29 14:52:06 +0200

    pom.xml updated (PostGIS) QQueryLab class added and implemented GeopackageTutorial updated (hasn't finsihed yet)

--- src/main/java/org/geotools/tutorial/quickstart/GeopackageTutorial.java
+++ src/main/java/org/geotools/tutorial/quickstart/GeopackageTutorial.java
@@ -4,10 +4,12 @@ import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureSource;
-import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.type.AttributeType;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GeopackageTutorial {
@@ -16,7 +18,7 @@ public class GeopackageTutorial {
         Map<String, String> geopackageParameters = new HashMap<>();
 
         geopackageParameters.put("dbtype", "geopkg");
-        geopackageParameters.put("database", "E:\\feren\\Development\\Java\\geotoolstutorials\\quickstart\\src\\main\\resources\\locations.gpkg");
+        geopackageParameters.put("database", "src/main/resources/locations.gpkg");
 
         DataStore dataStore;
         SimpleFeatureSource featureSource;
@@ -28,14 +30,22 @@ public class GeopackageTutorial {
                 throw new IllegalStateException("Couldn't connect! Check parameters!");
             }
             featureSource = dataStore.getFeatureSource("locations");
+            String crs = featureSource.getSchema().getCoordinateReferenceSystem().toString();
+            List<AttributeType> fields = featureSource.getSchema().getTypes();
+            List<String> fieldNames = new ArrayList<>();
+            for (AttributeType actual : fields) {
+                fieldNames.add(actual.getName().toString());
+            }
+
             featureCollection = featureSource.getFeatures();
         } catch (IOException ioe) {
             throw new IllegalStateException("Cannot read datastore: " + geopackageParameters.get("database") + "!");
         }
-
-        while (featureCollection.features().hasNext()) {
-            SimpleFeature feature = featureCollection.features().next();
-            System.out.println(feature.getDefaultGeometry().toString());
-        }
+//
+//        while (featureCollection.features().hasNext()) {
+//            SimpleFeature feature = featureCollection.features().next();
+//            System.out.println(feature.toString());
+//            System.out.println(feature.getDefaultGeometry().toString());
+//        }
     }
 }
